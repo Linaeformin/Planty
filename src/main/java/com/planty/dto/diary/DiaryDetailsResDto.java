bcf71c4b7b8851c93e8d948b61bf420a98112316
Linaeformin <minsa030407@gmail.com>
2025-09-03 02:56:13 +0900

    chore(diary, board): 겹치는 DTO 삭제 및 time 반환

--- src/main/java/com/planty/dto/diary/DiaryDetailsResDto.java
+++ src/main/java/com/planty/dto/diary/DiaryDetailsResDto.java
@@ -1,20 +1,22 @@
 package com.planty.dto.diary;
 
-import com.planty.entity.diary.DiaryImage;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.util.List;
 
 
-// 재배 일지 상세 페이지 프론트에게 전달
+// 판매 게시글 -> 재배 일지 프론트에게 전달하는 DTO
 @Getter @Setter
 @Builder
 public class DiaryDetailsResDto {
     private Integer diaryId;
     private String title;
     private String content;
+    private List<String> images;
+    private String time;
     private String analysis;
-    private List<DiaryImageDto> images;
     private Boolean isOwner;
 }
+
