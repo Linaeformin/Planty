c8844cb574b4a6492aa49d912dd9fea7847406a0
Linaeformin <minsa030407@gmail.com>
2025-09-04 23:39:08 +0900

    remove(chat, AIchat): 재구성할 파일 전체 삭제

--- src/main/java/com/planty/repository/chat/ChatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.planty.repository.chat;
-
-import com.planty.entity.chat.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<Chat, Long> {
-}
